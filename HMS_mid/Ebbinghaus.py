b88cb6e8c176a2395d4c8eb51bc773f2b595e5ec
isogin <daxiangy147@gmail.com>
2024-06-27 09:47:21 +0900

    エビングハウス錯視の仮実装。 #周りの円の大きさが変わってしまっている状態

--- HMS_mid/Ebbinghaus.py
+++ HMS_mid/Ebbinghaus.py
@@ -1,21 +1,13 @@
 # import
 import math
 import random
-import pathlib
-import numpy as np  # whole numpy lib is available, prepend "np."
+import numpy as np
 from numpy import (sin, cos, tan, log, log10, pi, average,
-                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
-import os  # handy system and path functions
-import sys  # to get file system encoding
-from psychopy import locale_setup
-from psychopy import prefs
-from psychopy import sound, gui, visual, core, data, event, logging, clock
-from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
-                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
-from psychopy.hardware import keyboard
+				   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+import os
+from psychopy import gui, visual, core, data, event, logging
 
 # ファイル準備
-# Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_thisDir)
 
@@ -30,7 +22,7 @@ new_filepath = os.path.join(".", "data", new_filename)
 
 # save a log file for detail verbose info
 logFile = logging.LogFile(new_filepath + ".log", level=logging.EXP)
-logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+logging.console.setLevel(logging.WARNING)
 csvFile = new_filepath + ".csv"
 
 # ファイルが存在する場合はキャンセル
@@ -38,15 +30,15 @@ if os.path.isfile(csvFile):
     print(csvFile + " exists!")
     core.quit()
 else:
-    datafile = open(csvFile, mode="x", encoding='utf-8')  # mode = "a"だと追記可能，"x"だと新規作成のみ可能
-    datafile.write("central_size,flanker_size,response,reaction_time\n")  # 列名をつけておく
+    datafile = open(csvFile, mode="x", encoding='utf-8')
+    datafile.write("left_flanker_size,right_flanker_size,response,reaction_time\n")
 
 # windowの準備
 win = visual.Window(
-    monitor="testMonitor",  # 自分で設定したモニター設定名を読み込む
-    units="deg",  # 表示する単位，ここでは視野角
-    fullscr=True,  # フルスクリーン表示
-    color=[0, 0, 0]  # 色強度は-1.0から1.0の実数で指定
+    monitor="testMonitor",
+    units="deg",
+    fullscr=True,
+    color=[0, 0, 0]
 )
 
 # 円の準備
@@ -67,7 +59,7 @@ flanker_circle = visual.Circle(
 # マウスカーソル設定
 mouse = event.Mouse(
     win=win,
-    visible=0  # 1で表示，0で非表示
+    visible=0
 )
 
 # 中央の円の基準サイズ
@@ -100,64 +92,71 @@ trial_ID = 0
 stopwatch = core.Clock()
 
 for delta_size_cond in delta_size_cond_order:
-    flanker_size = size_cond[delta_size_cond]
+    left_flanker_size = size_cond[delta_size_cond]
+    right_flanker_size = size_cond[delta_size_cond_order[(trial_ID + cond_num // 2) % cond_num]]
     print(trial_ID)
-    print(flanker_size)
+    print(left_flanker_size, right_flanker_size)
 
     # 実験内容の説明
     if trial_ID == 0:
-        inst = visual.ImageStim(win, "inst.png")  # 1. 画像刺激の準備
+        inst = visual.ImageStim(win, "inst.png")
         inst.size = (21, 7)
         inst.draw()
         win.flip()
         event.waitKeys(keyList=["space"])
 
-    # 中央円の描画
+    # 左側のエビングハウス錯視の描画
+    for pos in flanker_positions:
+        flanker_circle.radius = left_flanker_size
+        flanker_circle.pos = (-8 + pos[0], pos[1])
+        flanker_circle.draw()
+
     central_circle.radius = central_size_L0
-    central_circle.pos = (0, 0)
+    central_circle.pos = (-8, 0)
     central_circle.draw()
 
-    # フランカー円の描画
+    # 右側のエビングハウス錯視の描画
     for pos in flanker_positions:
-        flanker_circle.radius = flanker_size
-        flanker_circle.pos = pos
+        flanker_circle.radius = right_flanker_size
+        flanker_circle.pos = (8 + pos[0], pos[1])
         flanker_circle.draw()
+
+    central_circle.radius = central_size_L0
+    central_circle.pos = (8, 0)
+    central_circle.draw()
 
     # 画面に反映
     win.flip()
 
     # 反応時間記録のために時間を0にリセット
     stopwatch.reset()
-    resp = event.waitKeys(keyList=["up", "down", "escape"], timeStamped=stopwatch)
+    resp = event.waitKeys(keyList=["left", "right", "escape"], timeStamped=stopwatch)
 
     # データの保存
-    key = resp[0][0]  # 反応キーの取得
-    rt = resp[0][1]  # 反応時間の取得
+    key = resp[0][0]
+    rt = resp[0][1]
     print(key)
     print(rt)
 
-    # check for quit (typically the Esc key)
     if key == "escape":
         break
 
-    # 解答はフランカー円が大きいか小さいかで記録する
-    if key == "up":
-        answer = "larger"
+    # 解答は左の錯視か右の錯視のどちらが中央円が大きいかで記録する
+    if key == "left":
+        answer = "left"
     else:
-        answer = "smaller"
+        answer = "right"
 
-    # 結果をファイルに書き込む
-    data = "{},{},{},{}\n".format(central_size_L0, flanker_size, answer, rt)  # カンマ区切りの文字列にする
-    datafile.write(data)  # ファイルに書き込む
+    data = "{},{},{},{}\n".format(left_flanker_size, right_flanker_size, answer, rt)
+    datafile.write(data)
 
-    trial_ID = trial_ID + 1  # ループごとにtrial_IDを1増やす
+    trial_ID += 1
 
     # 注視点の表示
     win.flip()
-    # 250 ms待機
     core.wait(0.5)
 
 # ループ外で終了処理
-datafile.close()  # ファイルを閉じる
-win.close()  # 画面を閉じる
+datafile.close()
+win.close()
 core.quit()
